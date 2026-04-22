a81e87eea8abe080b36828fdefe58214d88dfc1d
moazabdelmoneim <moazabdelmoneim315@gmail.com>
2026-04-22 21:37:05 +0200

    i forget to put the new db uri

--- src/DB/db.connection.js
+++ src/DB/db.connection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const connectDB = async()=>{
 
-	await mongoose.connect('mongodb://localhost:27017/mongooseAssingment').then(
+	await mongoose.connect('mongodb+srv://moazmnm:1234@cluster0.6nk1p6s.mongodb.net/mongooseVercel').then(
 		result=>{
 			console.log('db connected');
 		}
